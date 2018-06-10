cbd1f0c153e2d2190e9799ea0fd3e35eabd3903b
Arjun Nair <aqube@arjuns-air.attlocal.net>
2018-06-09 23:05:22 -0500

    Restaurant layout.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -97,6 +97,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
   const title = document.createElement('h2');
   title.innerHTML = 'Reviews';
+  title.setAttribute("class","reviewTitle");
   container.appendChild(title);
 
   if (!reviews) {
