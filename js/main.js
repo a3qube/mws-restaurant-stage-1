d14217e1af32d5ade75b8bfca50ec1712696a2d7
Arjun Nair <aqube@arjuns-air.attlocal.net>
2018-06-09 19:57:30 -0500

    Main page layout fixed

--- js/main.js
+++ js/main.js
@@ -140,24 +140,31 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = "Restaurant Image - "+restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
-  name.innerHTML = restaurant.name;
+  const name = document.createElement('h2');
+  name.innerHTML = "  "+restaurant.name;
+  name.setAttribute("class", "fontawesome-food");
+  // name.class = 'fontawesome-food';
   li.append(name);
+  const div = document.createElement('div');
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  neighborhood.setAttribute("class", "typicons-compass");
+  div.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  div.append(address);
 
-  const more = document.createElement('a');
+  li.append(div);
+  const more = document.createElement('button');
   more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
+  more.onclick = () => {window.location = DBHelper.urlForRestaurant(restaurant);};
+  // more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
   return li
