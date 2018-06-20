cadd6b70e08e5e80b555b44e5a166e09f79403bd
Arjun Nair <aqube@arjuns-air.attlocal.net>
2018-06-19 21:28:21 -0500

    Review 2

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -129,7 +129,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   title.setAttribute("class","reviewTitle");
   container.appendChild(title);
